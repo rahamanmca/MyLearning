369f048d716300cedc5df36913dd34323157a57e
rahamanmca <65031194+rahamanmca@users.noreply.github.com>
2020-05-08 23:05:48 +0530

    Update Helloworld.java

--- Helloworld.java
+++ Helloworld.java
@@ -2,7 +2,7 @@ public class Helloworld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hello Java");
+		System.out.println("hello Java abdul!!!");
 
 	}
 
