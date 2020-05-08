c3f111049fb617137b49ccd77aa29bb152edf028
rahamanmca <65031194+rahamanmca@users.noreply.github.com>
2020-05-08 22:37:22 +0530

    java file added
    java file added

---
+++ Helloworld.java
@@ -0,0 +1,11 @@
+package com.fedex.test.jenkins.examples;
+
+public class Helloworld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello Java");
+
+	}
+
+}
